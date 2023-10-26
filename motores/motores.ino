e580bea97a423c9e597891237469fc0ca4eed32f
Artur Bristot <128424203+arturbristot@users.noreply.github.com>
2023-10-26 16:23:37 -0300

    Update motores.ino

--- motores/motores.ino
+++ motores/motores.ino
@@ -1,57 +1,68 @@
-int m1 = 2;
-int m2 = 14;
-int m3 = 15;
-int m4 = 13;
-int spd = (12, 16);
-int velo;
+// Motor A
+const int motor1Pin1 = 2; 
+const int motor1Pin2 = 14; 
+const int enable1Pin = 12;
+
+// Motor B
+const int motor2Pin1 = 15; 
+const int motor2Pin2 = 13; 
+const int enable2Pin = 0;
+
+// configurando pwm
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int velocidade = 255;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(m1, OUTPUT);
-  pinMode(m2, OUTPUT);
-  pinMode(m3, OUTPUT);
-  pinMode(m4, OUTPUT);
-  pinMode(spd, OUTPUT);  
-  velo = 20;
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
+  pinMode(enable2Pin, OUTPUT);
+  
+  // configure LED PWM functionalitites
+  ledcSetup(pwmChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(enable1Pin, pwmChannel);
+  ledcAttachPin(enable2Pin, pwmChannel);
+
+  ledcWrite(pwmChannel, velocidade);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   frente();
-  
 }
 
 void frente(){
- analogWrite(spd, velo);
- digitalWrite(m1, 1);
- digitalWrite(m2, 0);
-  
- digitalWrite(m3, 1);
- digitalWrite(m4, 0);
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
 }
 
 void tras(){
- analogWrite(spd, velo);
- digitalWrite(m1, 0);
- digitalWrite(m2, 1);
-  
- digitalWrite(m3, 0);
- digitalWrite(m4, 1);
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
 }
 
 void gesq(){
- analogWrite(spd, velo);
- digitalWrite(m1, 1);
- digitalWrite(m2, 0);
-  
- digitalWrite(m3, 0);
- digitalWrite(m4, 1);
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
 }
 
 void gdir(){
- analogWrite(spd, velo);
- digitalWrite(m1, 0);
- digitalWrite(m2, 1);
-  
- digitalWrite(m3, 1);
- digitalWrite(m4, 0);
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, LOW);
+  digitalWrite(motor2Pin2, HIGH);
 }

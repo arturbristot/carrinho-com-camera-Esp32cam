1d8de92536f98d511578bcc74328957610f48612
Artur Bristot <128424203+arturbristot@users.noreply.github.com>
2023-11-09 17:21:39 -0300

    Update carrinho.ino

--- carrinho/carrinho.ino
+++ carrinho/carrinho.ino
@@ -3,11 +3,31 @@
 #include <SPIFFS.h>
 #include <WebSocketsServer.h>
 
+
+//motor
+// Motor A
+const int motor1Pin1 = 2;
+const int motor1Pin2 = 14;
+const int enable1Pin = 12;
+
+// Motor B
+const int motor2Pin1 = 15;
+const int motor2Pin2 = 13;
+const int enable2Pin = 0;
+
+// configurando pwm
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int velocidade = 255;
+//motor
+
 WebSocketsServer webSocket(81); // Porta WebSocket
 
 void handleWebSocketMessage(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
   if (type == WStype_TEXT) {
     Serial.printf("[%u] Text received: %s\n", num, payload);
+    frente();
   } else if (type == WStype_CONNECTED) {
     Serial.printf("[%u] WebSocket Connected\n", num);
   }
@@ -23,6 +43,24 @@ int btnState = false;
 AsyncWebServer server(80);
 
 void setup() {
+  //motor
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
+
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
+  pinMode(enable2Pin, OUTPUT);
+
+  // configure LED PWM functionalitites
+  ledcSetup(pwmChannel, freq, resolution);
+
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(enable1Pin, pwmChannel);  
+  ledcAttachPin(enable2Pin, pwmChannel);
+
+  ledcWrite(pwmChannel, velocidade);
+  //motor
   Serial.begin(115200);
   delay(10);
 
@@ -108,4 +146,32 @@ void loop() {
     }
     delay(1000);
   }
+}
+
+void frente(){
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin2, HIGH);
+  digitalWrite(motor2Pin1, LOW);
+}
+
+void tras(){
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin2, LOW);
+  digitalWrite(motor2Pin1, HIGH);
+}
+
+void gesq(){
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+  digitalWrite(motor2Pin2, HIGH);
+  digitalWrite(motor2Pin1, LOW);
+}
+
+void gdir(){
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin2, LOW);
+  digitalWrite(motor2Pin1, HIGH);
 }

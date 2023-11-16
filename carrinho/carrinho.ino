5c1f665dc9ba02c7702347f488f33fec7129673c
Artur Bristot <128424203+arturbristot@users.noreply.github.com>
2023-11-16 16:18:20 -0300

    Update carrinho.ino

--- carrinho/carrinho.ino
+++ carrinho/carrinho.ino
@@ -1,10 +1,12 @@
+#include <ESP32Servo.h>
+#include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 #include <WebSocketsServer.h>
 
+// configs do motor
 
-//motor
 // Motor A
 const int motor1Pin1 = 2;
 const int motor1Pin2 = 14;
@@ -20,30 +22,43 @@ const int freq = 30000;
 const int pwmChannel = 0;
 const int resolution = 8;
 int velocidade = 255;
-//motor
-
-WebSocketsServer webSocket(81); // Porta WebSocket
-
-void handleWebSocketMessage(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
-  if (type == WStype_TEXT) {
-    Serial.printf("[%u] Text received: %s\n", num, payload);
-    frente();
-  } else if (type == WStype_CONNECTED) {
-    Serial.printf("[%u] WebSocket Connected\n", num);
-  }
-}
-
-
-const char* ssid = "SATC IOT";
-const char* password = "IOT2023@";
 
 int btnGPIO = 0;
 int btnState = false;
 
+const char *ssid = "Carrinho";
+const char *password = "Carrinho";
+
 AsyncWebServer server(80);
 
+
+WebSocketsServer webSocket(81); // Porta WebSocket
+void handleWebSocketMessage(uint8_t num, WStype_t type, uint8_t *payload, size_t length) {
+  if (type == WStype_TEXT) {   
+    if (strcmp((const char*)payload, "frente") == 0) {
+      Serial.println("frente!");
+      frente();
+    }
+    else if (strcmp((const char*)payload, "gesq") == 0){
+      Serial.println("gesq!");
+      gesq();
+    }
+    else if (strcmp((const char*)payload, "gdir") == 0){
+      Serial.println("gdir!");
+      gdir();
+    }
+    else if (strcmp((const char*)payload, "tras") == 0){
+      Serial.println("tras!");
+      tras();
+    }
+    else if (strcmp((const char*)payload, "parado") == 0) {
+      Serial.println("parado!");
+      parado();
+    }
+}
+}
+
 void setup() {
-  //motor
   pinMode(motor1Pin1, OUTPUT);
   pinMode(motor1Pin2, OUTPUT);
   pinMode(enable1Pin, OUTPUT);
@@ -66,74 +81,30 @@ void setup() {
 
   pinMode(btnGPIO, INPUT);
 
-  Serial.println();
-  Serial.print("[WiFi] Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  int tryDelay = 500;
-  int numberOfTries = 20;
-
-  while (true) {
-    switch (WiFi.status()) {
-      case WL_NO_SSID_AVAIL:
-        Serial.println("[WiFi] SSID not found");
-        break;
-      case WL_CONNECT_FAILED:
-        Serial.print("[WiFi] Failed - WiFi not connected! Reason: ");
-        return;
-      case WL_CONNECTION_LOST:
-        Serial.println("[WiFi] Connection was lost");
-        break;
-      case WL_SCAN_COMPLETED:
-        Serial.println("[WiFi] Scan is completed");
-        break;
-      case WL_DISCONNECTED:
-        Serial.println("[WiFi] WiFi is disconnected");
-        break;
-      case WL_CONNECTED:
-        Serial.println("[WiFi] WiFi is connected!");
-        Serial.print("[WiFi] IP address: ");
-        Serial.println(WiFi.localIP());
-        setupServer();
-        return;
-      default:
-        Serial.print("[WiFi] WiFi Status: ");
-        Serial.println(WiFi.status());
-        break;
-    }
-    delay(tryDelay);
-
-    if (numberOfTries <= 0) {
-      Serial.print("[WiFi] Failed to connect to WiFi!");
-      WiFi.disconnect();
-      return;
-    } else {
-      numberOfTries--;
-    }
-  }
-  server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
-  webSocket.begin();
-  webSocket.onEvent(handleWebSocketMessage);
-}
-
-void setupServer() {
   if (!SPIFFS.begin(true)) {
     Serial.println("Falha ao montar o sistema de arquivos SPIFFS.");
     return;
   }
+
+  WiFi.softAP(ssid, password);
+  IPAddress IP = WiFi.softAPIP();
+  Serial.begin(115200);
+  Serial.print("Endereço IP do AP: ");
+  Serial.println(IP);
+
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/index.html", "text/html");
   });
   server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SPIFFS, "/script.js", "application/javascript");
   });
+
   server.begin();
-  
+
   webSocket.begin();
   webSocket.onEvent(handleWebSocketMessage);
 }
+
 
 void loop() {
   webSocket.loop();
@@ -146,6 +117,13 @@ void loop() {
     }
     delay(1000);
   }
+}
+
+void parado(){
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin2, LOW);
+  digitalWrite(motor2Pin1, LOW);
 }
 
 void frente(){
@@ -175,3 +153,4 @@ void gdir(){
   digitalWrite(motor2Pin2, LOW);
   digitalWrite(motor2Pin1, HIGH);
 }
+
